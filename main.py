57bf093304586748aa36fb09bae2cf00be6f3290
Anita Chen <achen1105@gmail.com>
2020-12-06 18:57:31 -0800

    added message commands

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ token = os.getenv('DISCORD_BOT_TOKEN')
 async def on_ready() :
     print("I am online")
 
+@client.event
+async def on_message(message):
+    # Hello uncle roger
+    if message.content.find("hello uncle roger") != -1:
+        await message.channel.send(f'Hallo nieces and nephews! :rice:');
+    if message.content.find("egg fried rice") != -1:
+        await message.channel.send(f'Make sure to use wok for the wok hei :cooking:');
+
 """ 
 @client.command(aliases=['addmats', 'add mats'])
 async def mats(ctx, *, materials):
