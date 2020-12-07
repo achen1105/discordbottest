669d1b73cc7fd1570e84f45ee273d61c5503d9b8
Anita Chen <achen1105@gmail.com>
2020-12-06 17:56:42 -0800

    First commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 17:36:41 2020
+
+@author: anitachen
+"""
+
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix=".")
+#token = os.getenv("DISCORD_BOT_TOKEN")
+
+@client.event
+async def on_ready() :
+    print("I am online")
+
+""" 
+@client.command(aliases=['addmats', 'add mats'])
+async def mats(ctx, *, materials):
+    await ctx.send(f'test added {materials}');
+    print("added mats");
+    
+@client.command()
+async def materials(ctx, arg1: int, arg2: int):
+    #add reaction to message
+    await ctx.send(f'added mats {arg1} and {arg2} :heart:');
+    print("added mats");
+"""
+
+client.run("Nzg1MzIwMjc3Mjk4MjQ5NzM4.X82IgA.5D5f4J7pvuX5w3JxUl6m0t3fw0g")
+
